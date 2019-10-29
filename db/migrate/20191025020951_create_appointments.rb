9284ccf4b091156b63bca5641e35028ecba36b10
unknown <quique_barahona5@hotmail.com>
2019-10-28 22:10:13 -0500

    added formating for all pages and changed date format for appointments

--- db/migrate/20191025020951_create_appointments.rb
+++ db/migrate/20191025020951_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
-      t.datetime :visit
+      t.date_time :visit
       t.references :physician, null: false, foreign_key: true
       t.references :patient, null: false, foreign_key: true
 
