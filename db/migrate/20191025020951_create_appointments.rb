758ad70eab8e9ec0034f526a070492e735f108ae
dbara2 <quique_barahona5@hotmail.com>
2019-11-05 21:10:16 -0600

    fixed migration and appointments table

--- db/migrate/20191025020951_create_appointments.rb
+++ db/migrate/20191025020951_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
-      t.date_time :visit
+      t.datetime :visit
       t.references :physician, null: false, foreign_key: true
       t.references :patient, null: false, foreign_key: true
 
